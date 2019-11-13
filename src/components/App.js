483d9f22d574aae9d0222e583d50556bde617fbd
Anthony Messager <anthony.messager@MBP-C02YX52CLVDL.leboncoin.loc>
2019-11-13 13:55:31 +0100

    First commit with header component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="uk-section uk-section-muted uk-section-small">
+        <div className="uk-container">Content</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
